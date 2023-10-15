925a56238b73643a330be5b2e248d1ea885b3d3d
aa46 <atestemail1229@163.com>
2023-10-15 12:12:47 +0800

    Add files via upload

--- build_pyinstaller.bat
+++ build_pyinstaller.bat
@@ -1,5 +1,5 @@
 python -m pip install -r requirements.txt
 echo 初始化一些东西，请关闭待会弹出的窗口
 python seewoedit.py
-python -m PyInstaller -F -w --uac-admin seewoedit.py --disable-console --enable-plugin=pyside6 --enable-plugin=upx -i %appdata%\a.ico
+python -m PyInstaller -F -w --uac-admin seewoedit.py -i %appdata%\a.ico
 move dist\seewoedit.exe swenlauncher.exe
