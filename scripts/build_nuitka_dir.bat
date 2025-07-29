36c00f33dde90385ceec4fe59c11c8df81efb29b
Git-Learn1 <atestemail1229@gmail.com>
2025-07-29 14:48:12 +0800

    Change some files' directory to fit the new struct

--- scripts/build_nuitka_dir.bat
+++ scripts/build_nuitka_dir.bat
@@ -1,7 +1,1 @@
-python -m pip install -r requirements.txt
-echo 初始化一些东西，请关闭待会弹出的窗口
-python seewoedit.py
-taskkill /f /im EasiNote.exe /t
-taskkill /f /im EasiNote5.exe /t
-echo 如果编译失败，请安装Visual Studio 2022并打开x64 Native Tools Command Prompt for VS 2022，后运行此脚本
-python -m nuitka --standalone --msvc=14.3 seewoedit.py --output-filename="swenlauncher.exe" --disable-console --enable-plugin=pyside6 --enable-plugin=upx --windows-icon-from-ico=se\data\inside\a.ico
+python -m nuitka --standalone --msvc=14.3 ..\src\main.py --output-filename="swenlauncher.exe" --disable-console --enable-plugin=pyside6 --enable-plugin=upx --windows-icon-from-ico=..\src\datas\builtin\a.ico
--- scripts/build_pyinstaller.bat
+++ scripts/build_pyinstaller.bat
@@ -1,7 +1,1 @@
-python -m pip install -r requirements.txt
-echo 初始化一些东西，请关闭待会弹出的窗口
-python seewoedit.py
-taskkill /f /im EasiNote.exe /t
-taskkill /f /im EasiNote5.exe /t
-python -m PyInstaller -F -w seewoedit.py -i se\data\inside\a.ico
-move dist\seewoedit.exe swenlauncher.exe
+python -m PyInstaller -F -w ..\src\main -i ..\src\datas\builtin\a.ico
